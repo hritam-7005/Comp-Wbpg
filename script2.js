97cee8ec6511c2a813a9c801fb4cf367a8e3e953
Hritam <98631748+hritam-7005@users.noreply.github.com>
2022-05-26 12:07:02 +0530

    Update

--- script2.js
+++ script2.js
@@ -1,7 +1,7 @@
 function load() {
     var overlay = document.getElementById("loading-overlay");
     var text = document.getElementById("load-overlay-text");
-    text.innerHTML ="Welcome";
+    //text.innerHTML ="<.../>";
     setTimeout(function() {
         text.style.transform="scale(1)";
         text.style.opacity="0.5";
@@ -9,4 +9,96 @@ function load() {
             overlay.style.transform="translate(-100%)";
         }, 800);
     }, 500)
+}
+//Navigation
+function homeNavigate() {
+    var text = document.getElementById("nav-overlay-text");
+    var overlay = document.getElementById("navigation-overlay");
+    overlay.style.transform="translate(0%)";
+    text.innerHTML ="Home"
+    setTimeout(function() {
+        text.style.transform="scale(1.5)";
+        overlay.style.backgroundColor="rgba(0, 0, 0, 1)";
+        setTimeout(function() {
+            location.replace("./index.html");
+        }, 1000);
+    }, 1000);
+}
+function classNavigate1() {
+    var text = document.getElementById("nav-overlay-text");
+    var overlay = document.getElementById("navigation-overlay");
+    overlay.style.transform="translate(0%)";
+    text.innerHTML ="Class VII"
+    setTimeout(function() {
+        text.style.transform="scale(1.5)";
+        overlay.style.backgroundColor="rgba(0, 0, 0, 1)";
+        setTimeout(function() {
+            location.replace("./Section1.html");
+        }, 1000);
+    }, 1000);
+}
+function classNavigate2() {
+    var text = document.getElementById("nav-overlay-text");
+    var overlay = document.getElementById("navigation-overlay");
+    overlay.style.transform="translate(0%)";
+    text.innerHTML ="Class VIII"
+    setTimeout(function() {
+        text.style.transform="scale(1.5)";
+        overlay.style.backgroundColor="rgba(0, 0, 0, 1)";
+        setTimeout(function() {
+            location.replace("./Section2.html");
+        }, 1000);
+    }, 1000);
+}
+function classNavigate3() {
+    var text = document.getElementById("nav-overlay-text");
+    var overlay = document.getElementById("navigation-overlay");
+    overlay.style.transform="translate(0%)";
+    text.innerHTML ="Class IX"
+    setTimeout(function() {
+        text.style.transform="scale(1.5)";
+        overlay.style.backgroundColor="rgba(0, 0, 0, 1)";
+        setTimeout(function() {
+            location.replace("./Section3.html");
+        }, 1000);
+    }, 1000);
+}
+function classNavigate4() {
+    var text = document.getElementById("nav-overlay-text");
+    var overlay = document.getElementById("navigation-overlay");
+    overlay.style.transform="translate(0%)";
+    text.innerHTML ="Class X"
+    setTimeout(function() {
+        text.style.transform="scale(1.5)";
+        overlay.style.backgroundColor="rgba(0, 0, 0, 1)";
+        setTimeout(function() {
+            location.replace("./Section4.html");
+        }, 1000);
+    }, 1000);
+}
+function classNavigate5() {
+    var text = document.getElementById("nav-overlay-text");
+    var overlay = document.getElementById("navigation-overlay");
+    overlay.style.transform="translate(0%)";
+    text.innerHTML ="Class XI"
+    setTimeout(function() {
+        text.style.transform="scale(1.5)";
+        overlay.style.backgroundColor="rgba(0, 0, 0, 1)";
+        setTimeout(function() {
+            location.replace("./Section5.html");
+        }, 1000);
+    }, 1000);
+}
+function classNavigate6() {
+    var text = document.getElementById("nav-overlay-text");
+    var overlay = document.getElementById("navigation-overlay");
+    overlay.style.transform="translate(0%)";
+    text.innerHTML ="Class XII"
+    setTimeout(function() {
+        text.style.transform="scale(1.5)";
+        overlay.style.backgroundColor="rgba(0, 0, 0, 1)";
+        setTimeout(function() {
+            location.replace("./Section6.html");
+        }, 1000);
+    }, 1000);
 }
